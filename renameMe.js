ccad0e16dded2b9c3fb122c3f985bb0ea975c0b9
7Dan2 <unic.norton@gmail.com>
2019-02-16 00:28:21 +0100

    testing some input stuff ... dirty

--- renameMe.js
+++ renameMe.js
@@ -1,10 +1,30 @@
-function renameMe(){
-    let docincaz = document.getElementById("chooseFileCAZ");
-    let docincaz0 = document.getElementById("chooseFileCAZ0");
-    let docinpi = document.getElementById("chooseFilePI");
-    console.log(docincaz, docincaz0, docinpi);
-}
-//A adapter et tester
-var file = document.GetElementById('fileupload1');
-var blob = file.files[0].slice(0, file.files[0].size, 'image/png'); 
-newFile = new File([blob], 'name.png', {type: 'image/png'});
+//function renameMe(){
+    //let docincaz = document.getElementById("chooseFileCAZ").files;
+    //let oFiles = docincaz.name;
+    //let files = 0;
+    //let out = File.name;
+    //for (var i = 0, numFiles = files.length; i < numFiles; i++) {
+    //    var file = files[i];
+        
+    //  }
+      
+    //let blob = new Blob;
+    //blob = docincaz.docincaz[0].slice(0, docincaz.docincaz[0].size, 'application/pdf'); 
+    //newFile = new File([blob], 'renamed.png', {type: 'application/pdf'});
+
+    /*let docincaz0 = document.getElementById("chooseFileCAZ0").files[0];
+    let docinpi = document.getElementById("chooseFilePI").files[0];
+    let docindue = document.getElementById("chooseFileDUE").files[0];
+    console.log(docincaz, oFiles, out);
+    
+    //document.getElementById("fileSizeOut") = ;
+
+//var file = document.GetElementById('chooszFileCAZ');//
+
+
+}*/
+const input = document.querySelector('input[type="file"]')
+input.addEventListener('input', function (e){
+    console.log(input.files)
+},false)
+//}
---
+++ text.js
@@ -0,0 +1,52 @@
+window.onload = function() {
+
+    var fileInput = document.getElementById('fileInput');
+    var fileDisplayArea = document.getElementById('fileDisplayArea');
+
+
+    fileInput.addEventListener('change', function(e) {
+        var file = fileInput.files[0];
+        var imageType = /image.*/;
+
+        if (file.type.match(imageType)) {
+            var reader = new FileReader();
+
+            reader.onload = function(e) {
+                fileDisplayArea.innerHTML = "";
+
+                var img = new Image();
+                img.src = reader.result;
+
+                fileDisplayArea.appendChild(img);
+            }
+
+            reader.readAsDataURL(file);	
+        } else {
+            fileDisplayArea.innerHTML = "File not supported!"
+        }
+    });
+
+}
+
+//text
+window.onload = function() {
+    var fileInput = document.getElementById('fileInput2');
+    var fileDisplayArea = document.getElementById('fileDisplayArea2');
+
+    fileInput.addEventListener('change', function(e) {
+        var file = fileInput.files[0];
+        var textType = /text.*/;
+
+        if (file.type.match(textType)) {
+            var reader = new FileReader();
+
+            reader.onload = function(e) {
+                fileDisplayArea.innerText = reader.result;
+            }
+
+            reader.readAsText(file);	
+        } else {
+            fileDisplayArea.innerText = "File not supported!"
+        }
+    });
+}
